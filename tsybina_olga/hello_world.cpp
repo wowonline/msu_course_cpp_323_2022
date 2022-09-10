bfbd686ef539d3b5c9a9f447e85cc8388f76e9ff
Jedi314 <olga.tsybina82@gmail.com>
2022-09-10 09:59:47 +0000

    Add hello_world app

---
+++ tsybina_olga/hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "Tsybina Olga" << std::endl;
+  std::cout << "Цыбина Ольга" << std::endl;
+  return 0;
+}
