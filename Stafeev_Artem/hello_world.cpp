58249e96be21f4d72ae8d73de0d30e400212cc10
jecteroid <tema.stafeev@yandex.ru>
2022-09-11 01:52:14 +0300

    Add hello_world app

---
+++ Stafeev_Artem/hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "Stafeev Artem" << std::endl;
+  std::cout << "Стафеев Артем" << std::endl;
+  return 0;
+}
