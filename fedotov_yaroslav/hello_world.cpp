3e5899b5a536b84532e44f144871aa18b99d7409
Huszar <yaroslavfedotov22@yandex.ru>
2022-09-11 21:50:12 +0300

    remove binary hello_world

--- fedotov_yaroslav/hello_world.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-  std::cout << "Hello World!" << std::endl;
-  std::cout << "Fedotov Yaroslav" << std::endl;
-  std::cout << "Федотов Ярослав" << std::endl;
-  return 0;
-}
-
