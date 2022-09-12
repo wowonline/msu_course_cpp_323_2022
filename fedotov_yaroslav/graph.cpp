91692c149b03a5bff320c64b25fb3960ee605edf
Huszar <yaroslavfedotov22@yandex.ru>
2022-09-12 09:26:52 +0300

    implemented basic graph interface

---
+++ fedotov_yaroslav/graph.cpp
@@ -0,0 +1,76 @@
+#include <vector>
+
+class Graph {
+  using VertexId = int;
+  using EdgeId = int;
+
+ public:
+  void add_vertex() { vertices_.emplace_back(last_vertex_id_++); }
+
+  void add_edge(VertexId first, VertexId second) {
+    edges_.emplace_back(last_edge_id_++,first, second);
+  }
+
+ private:
+  struct Vertex {
+   public:
+    explicit Vertex(VertexId id) : id_(id) {}
+    VertexId id() const { return id_; }
+
+   private:
+    VertexId id_ = 0;
+  };
+
+  struct Edge {
+   public:
+    Edge(EdgeId id, VertexId from_vertex_id, VertexId to_vertex_id)
+        : id_(id),
+          from_vertex_id_(from_vertex_id),
+          to_vertex_id_(to_vertex_id) {}
+
+    EdgeId id() const { return id_; }
+    VertexId from_vertex_id() const { return from_vertex_id_; }
+    VertexId to_vertex_id() const { return to_vertex_id_; }
+
+   private:
+    EdgeId id_ = 0;
+    VertexId from_vertex_id_ = 0;
+    VertexId to_vertex_id_ = 0;
+  };
+
+  std::vector<Vertex> vertices_;
+  std::vector<Edge> edges_;
+  VertexId last_vertex_id_;
+  EdgeId last_edge_id_;
+};
+
+constexpr int kVerticesCount = 13;
+
+int main() {
+  auto graph = Graph();
+
+  for (int i = 0; i < kVerticesCount; i++) {
+    graph.add_vertex();
+  }
+
+  graph.add_edge(0, 1);
+  graph.add_edge(0, 2);
+  graph.add_edge(0, 3);
+  graph.add_edge(1, 4);
+  graph.add_edge(1, 5);
+  graph.add_edge(1, 6);
+  graph.add_edge(2, 7);
+  graph.add_edge(2, 8);
+  graph.add_edge(3, 9);
+  graph.add_edge(4, 10);
+  graph.add_edge(5, 10);
+  graph.add_edge(6, 10);
+  graph.add_edge(7, 11);
+  graph.add_edge(8, 11);
+  graph.add_edge(9, 12);
+  graph.add_edge(10, 13);
+  graph.add_edge(11, 13);
+  graph.add_edge(12, 13);
+
+  return 0;
+}
