435ff720e0b8be8c80d7b16b2e035e4886e3111d
Jedi314 <olga.tsybina82@gmail.com>
2022-09-18 22:10:39 +0000

    fix formating

--- tsybina_olga/graph_generation.cpp
+++ tsybina_olga/graph_generation.cpp
@@ -1,43 +1,48 @@
 #include <vector>
 
 class Graph {
-  public:
-    using VertexId = int;
-    using EdgeId = int;
-    void add_vertex() {
-      vertices_.emplace_back(Vertex(get_new_vertex_id()));
-    };
+ public:
+  using VertexId = int;
+  using EdgeId = int;
+  void add_vertex() { vertices_.emplace_back(Vertex(get_new_vertex_id())); };
 
-    void add_edge(VertexId from_vertex_id, VertexId to_vertex_id) {
-      edges_.emplace_back(EdgeId(get_new_edge_id()), from_vertex_id, to_vertex_id);
-    };
-  private:
-    struct Vertex {
-      public:
-        explicit Vertex(VertexId id) : id_(id) {}
-        VertexId id() const { return id_; };
-      private:
-        VertexId id_ = 0;
-    };
-    struct Edge {
-      public:
-        explicit Edge(EdgeId id, VertexId from_vertex_id, VertexId to_vertex_id): id_(id), from_vertex_id_(from_vertex_id), to_vertex_id_(to_vertex_id) {}
-        EdgeId id() { return id_; }
-        VertexId from_vertex_id() const { return from_vertex_id_; };
-        VertexId to_vertex_id() const { return to_vertex_id_; };
-      private:
-        EdgeId id_ = 0;
-        VertexId from_vertex_id_ = 0;
-        VertexId to_vertex_id_ = 0;
-    };
-    std::vector<Vertex> vertices_;
-    std::vector<Edge> edges_;
-    
-    VertexId last_vertex_id_ = 0;
-    EdgeId last_edge_id_ = 0;
+  void add_edge(VertexId from_vertex_id, VertexId to_vertex_id) {
+    edges_.emplace_back(EdgeId(get_new_edge_id()), from_vertex_id,
+                        to_vertex_id);
+  };
 
-    VertexId get_new_vertex_id() { return last_vertex_id_++; }
-    EdgeId get_new_edge_id() { return last_edge_id_++; }
+ private:
+  struct Vertex {
+   public:
+    explicit Vertex(VertexId id) : id_(id) {}
+    VertexId id() const { return id_; };
+
+   private:
+    VertexId id_ = 0;
+  };
+  struct Edge {
+   public:
+    explicit Edge(EdgeId id, VertexId from_vertex_id, VertexId to_vertex_id)
+        : id_(id),
+          from_vertex_id_(from_vertex_id),
+          to_vertex_id_(to_vertex_id) {}
+    EdgeId id() { return id_; }
+    VertexId from_vertex_id() const { return from_vertex_id_; };
+    VertexId to_vertex_id() const { return to_vertex_id_; };
+
+   private:
+    EdgeId id_ = 0;
+    VertexId from_vertex_id_ = 0;
+    VertexId to_vertex_id_ = 0;
+  };
+  std::vector<Vertex> vertices_;
+  std::vector<Edge> edges_;
+
+  VertexId last_vertex_id_ = 0;
+  EdgeId last_edge_id_ = 0;
+
+  VertexId get_new_vertex_id() { return last_vertex_id_++; }
+  EdgeId get_new_edge_id() { return last_edge_id_++; }
 };
 
 int main() {
