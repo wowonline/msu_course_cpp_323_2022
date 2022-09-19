511bcb68a0a3ab8825eb934149e2ded3bf33b95b
klevvit <kle8it@gmail.com>
2022-09-19 10:08:13 +0300

    Keep edge vertices sorted

--- kovalenko_lev/edge.hpp
+++ kovalenko_lev/edge.hpp
@@ -1,19 +1,24 @@
 #pragma once
 
+#include <utility>
+
 #include "vertex.hpp"
 
 struct Edge {
  public:
   using Id = int;
 
-  Edge(Id id, Vertex::Id from, Vertex::Id to) : id_(id), from_(from), to_(to) {}
+  Edge(Id id, Vertex::Id from, Vertex::Id to) : id_(id), from_(from), to_(to) {
+    if (from > to) {
+        std::swap(from, to);
+    }
+  }
   Id id() const { return id_; }
   Vertex::Id from() const { return from_; }
   Vertex::Id to() const { return to_; }
 
   bool operator==(const Edge& rhs) const {
-    return (from_ == rhs.from_ && to_ == rhs.to_) ||
-           (from_ == rhs.to_ && to_ == rhs.from_);
+    return from_ == rhs.from_ && to_ == rhs.to_;
   }
 
   bool operator<(const Edge& rhs) const {
