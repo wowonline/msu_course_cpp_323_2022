3f68ee6bc69245b4b0f4b6dbe36485bb720cc909
Olga Tsybina <91283018+Jedi314@users.noreply.github.com>
2022-09-19 09:29:18 +0300

    Update graph_generation.cpp

--- tsybina_olga/graph_generation.cpp
+++ tsybina_olga/graph_generation.cpp
@@ -7,7 +7,7 @@ class Graph {
   void add_vertex() { vertices_.emplace_back(Vertex(get_new_vertex_id())); };
 
   void add_edge(VertexId from_vertex_id, VertexId to_vertex_id) {
-    edges_.emplace_back(EdgeId(get_new_edge_id()), from_vertex_id,
+    edges_.emplace_back(Edge(get_new_edge_id()), from_vertex_id,
                         to_vertex_id);
   };
 
