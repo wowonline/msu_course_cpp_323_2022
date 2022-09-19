307caaf97ad6d5d4c527f45b6c71f02060de5675
klevvit <kle8it@gmail.com>
2022-09-19 11:05:31 +0300

    Improve  data storage inside graph

--- kovalenko_lev/graph.hpp
+++ kovalenko_lev/graph.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <cassert>
+#include <map>
 #include <set>
 #include <vector>
 
@@ -9,24 +9,36 @@
 
 class Graph {
  public:
-  void add_vertex() { vertices_.emplace(vertices_.size()); }
+  void add_vertex() {
+    auto vertex_id = generate_vertex_id();
+    vertices_.emplace(vertex_id, vertex_id);
+  }
 
   void add_edge(Vertex::Id from, Vertex::Id to) {
-    assert(from >= 0 || from < vertices_.size());
-    assert(to >= 0 || to < vertices_.size());
-
-    if (edges_.find(Edge(0, from, to)) != edges_.end()) {
-      return;
-    }
-
-    edges_.emplace(edges_cnt_, from, to);
-    edges_.emplace(edges_cnt_, to, from);
-    ++edges_cnt_;
+    auto edge_id = generate_edge_id();
+    edges_.emplace(std::piecewise_construct, std::make_tuple(edge_id),
+                   std::make_tuple(edge_id, from, to));
+    connetions_[from].insert(edge_id);
+    connetions_[to].insert(edge_id);
   }
 
  private:
-  int edges_cnt_ = 0;
+  Vertex::Id generate_vertex_id() const {
+    Vertex::Id ans = next_vertex_id_;
+    next_vertex_id_ = Vertex::next_id(next_vertex_id_);
+    return ans;
+  }
 
-  std::set<Edge> edges_;
-  std::set<Vertex> vertices_;
+  Edge::Id generate_edge_id() const {
+    Edge::Id ans = next_edge_id_;
+    next_edge_id_ = Edge::next_id(next_edge_id_);
+    return ans;
+  }
+
+  mutable Vertex::Id next_vertex_id_ = 0;
+  mutable Edge::Id next_edge_id_ = 0;
+
+  std::map<Vertex::Id, Vertex> vertices_;
+  std::map<Edge::Id, Edge> edges_;
+  std::map<Vertex::Id, std::set<Edge::Id>> connetions_;
 };
