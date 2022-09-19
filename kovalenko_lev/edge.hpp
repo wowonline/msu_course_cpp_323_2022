36e34a5d7e5ec72c305a15f288fc36acd5f66fe6
klevvit <kle8it@gmail.com>
2022-09-19 09:23:32 +0300

    Move Edge out of graph

---
+++ kovalenko_lev/edge.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "vertex.hpp"
+
+struct Edge {
+ public:
+  using Id = int;
+
+  Edge(Id id, Vertex::Id from, Vertex::Id to) : id_(id), from_(from), to_(to) {}
+  Id id() const { return id_; }
+  Vertex::Id from() const { return from_; }
+  Vertex::Id to() const { return to_; }
+
+  bool operator==(const Edge& rhs) const {
+    return (from_ == rhs.from_ && to_ == rhs.to_) ||
+           (from_ == rhs.to_ && to_ == rhs.from_);
+  }
+
+  bool operator<(const Edge& rhs) const {
+    return from() == rhs.from() ? to() < rhs.to() : from() < rhs.from();
+  }
+
+  bool operator>(const Edge& rhs) const { return rhs < *this; }
+
+  bool operator>=(const Edge& rhs) const { return !operator<(rhs); }
+
+  bool operator<=(const Edge& rhs) const { return !operator>(rhs); }
+
+ private:
+  Id id_;
+  Vertex::Id from_;
+  Vertex::Id to_;
+};
--- kovalenko_lev/graph.hpp
+++ kovalenko_lev/graph.hpp
@@ -3,41 +3,11 @@
 #include <set>
 #include <vector>
 
+#include "edge.hpp"
 #include "vertex.hpp"
 
 class Graph {
  public:
-  using EdgeId = int;
-
-  struct Edge {
-   public:
-    Edge(EdgeId id, Vertex::Id from, Vertex::Id to)
-        : id_(id), from_(from), to_(to) {}
-    EdgeId id() const { return id_; }
-    Vertex::Id from() const { return from_; }
-    Vertex::Id to() const { return to_; }
-
-    bool operator==(const Edge& rhs) const {
-      return (from_ == rhs.from_ && to_ == rhs.to_) ||
-             (from_ == rhs.to_ && to_ == rhs.from_);
-    }
-
-    bool operator<(const Edge& rhs) const {
-      return from() == rhs.from() ? to() < rhs.to() : from() < rhs.from();
-    }
-
-    bool operator>(const Edge& rhs) const { return rhs < *this; }
-
-    bool operator>=(const Edge& rhs) const { return !operator<(rhs); }
-
-    bool operator<=(const Edge& rhs) const { return !operator>(rhs); }
-
-   private:
-    EdgeId id_;
-    Vertex::Id from_;
-    Vertex::Id to_;
-  };
-
   void add_vertex() { vertices_.emplace_back(vertices_.size()); }
 
   void add_edge(Vertex::Id from, Vertex::Id to) {
@@ -52,7 +22,7 @@ class Graph {
   }
 
  private:
-  EdgeId edges_cnt_ = 0;
+  int edges_cnt_ = 0;
 
   std::set<Edge> edges_;
   std::vector<Vertex> vertices_;
