b45edd439e30f7b075c005ccac8ed0e2adce1731
chereshnya123 <nikos08012@gmail.com>
2022-09-22 00:11:31 +0300

    Change graph.cpp

--- antyukhov_nikita/graph_gen.cpp
+++ antyukhov_nikita/graph_gen.cpp
@@ -3,9 +3,31 @@
 #include <vector>
 
 class Graph {
+ public:
   using VertexId = int;
   using EdgeId = int;
+  struct Vertex;
+  struct Edge;
+  void add_vertex() {
+    ++vertices_amount_;
+    VertexId new_id = get_new_vertex_id();
+    Vertex new_vertex(new_id);
+    vertices_vector_.push_back(new_vertex);
 
+    vertices_edges_map_[new_id] = {};
+  };
+
+  void add_edge(VertexId from_vertex_id, VertexId to_vertex_id) {
+    Edge new_edge1{get_new_edge_id(), from_vertex_id, to_vertex_id};
+    Edge new_edge2{get_new_edge_id(), to_vertex_id, from_vertex_id};
+    edges_vector_.push_back(new_edge1);
+    edges_vector_.push_back(new_edge2);
+
+    vertices_edges_map_[from_vertex_id].push_back(new_edge1);
+    vertices_edges_map_[to_vertex_id].push_back(new_edge2);
+  };
+
+ private:
   struct Vertex {
    public:
     explicit Vertex(VertexId id) : id_(id) {}
@@ -30,23 +52,16 @@ class Graph {
     VertexId to_vertex_id_ = 0;
   };
 
-  std::vector<std::vector<Edge>> vertices_vector_{};
+  VertexId get_new_vertex_id() { return vertices_amount_++; }
+
+  EdgeId get_new_edge_id() { return edges_amount_++; }
+
+  std::vector<Vertex> vertices_vector_ = {};
+  std::vector<Edge> edges_vector_ = {};
+  std::unordered_map<VertexId, std::vector<Edge>> vertices_edges_map_ = {};
+
   VertexId vertices_amount_ = 0;
   EdgeId edges_amount_ = 0;
-
- public:
-  void add_vertex() {
-    ++vertices_amount_;
-    std::vector<Edge> new_vertex{};
-    vertices_vector_.push_back(new_vertex);
-  };
-
-  void add_edge(VertexId from_vertex_id, VertexId to_vertex_id) {
-    Edge new_edge1{++edges_amount_, from_vertex_id, to_vertex_id};
-    Edge new_edge2{++edges_amount_, to_vertex_id, from_vertex_id};
-    vertices_vector_[from_vertex_id].push_back(new_edge1);
-    vertices_vector_[to_vertex_id].push_back(new_edge2);
-  };
 };
 
 int main() {
