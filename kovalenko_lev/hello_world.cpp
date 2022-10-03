6d38fb726ede7fb5b2c5e793fbcfc94a8fd2b77f
klevvit <kle8it@gmail.com>
2022-10-04 01:17:08 +0600

    Revert "Remove hello_world.cpp"
    This reverts commit afa3e923f808cc10260ce645d81e776dbd7cc2fe.

---
+++ kovalenko_lev/hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "Kovalenko Lev" << std::endl;
+  std::cout << "Коваленко Лев" << std::endl;
+  return 0;
+}
