404500ce5a5bff7e3d2e2d617e60e592e45c9a14
klevvit <kle8it@gmail.com>
2022-10-09 17:47:10 +0600

    remove unnecessary complexity

--- kovalenko_lev/edge.hpp
+++ kovalenko_lev/edge.hpp
@@ -9,31 +9,10 @@ struct Edge {
   using Id = int;
 
   Edge(Id id, Vertex::Id from_vertex_id, Vertex::Id to_vertex_id)
-      : id_(id), from_vertex_id_(from_vertex_id), to_vertex_id_(to_vertex_id) {
-    if (from_vertex_id > to_vertex_id) {
-      std::swap(from_vertex_id, to_vertex_id);
-    }
-  }
+      : id_(id), from_vertex_id_(from_vertex_id), to_vertex_id_(to_vertex_id) {}
   Id id() const { return id_; }
   Vertex::Id from_vertex_id() const { return from_vertex_id_; }
   Vertex::Id to_vertex_id() const { return to_vertex_id_; }
-
-  bool operator==(const Edge& rhs) const {
-    return from_vertex_id_ == rhs.from_vertex_id_ &&
-           to_vertex_id_ == rhs.to_vertex_id_;
-  }
-
-  bool operator<(const Edge& rhs) const {
-    return from_vertex_id() == rhs.from_vertex_id()
-               ? to_vertex_id() < rhs.to_vertex_id()
-               : from_vertex_id() < rhs.from_vertex_id();
-  }
-
-  bool operator>(const Edge& rhs) const { return rhs < *this; }
-
-  bool operator>=(const Edge& rhs) const { return !operator<(rhs); }
-
-  bool operator<=(const Edge& rhs) const { return !operator>(rhs); }
 
  private:
   Id id_;
