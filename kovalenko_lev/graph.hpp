56fe3b77c3911ebc7d04876ace30b5c72152fa41
klevvit <kle8it@gmail.com>
2022-10-09 18:41:46 +0600

    add !has_edge assertion

--- kovalenko_lev/graph.hpp
+++ kovalenko_lev/graph.hpp
@@ -20,12 +20,35 @@ class Graph {
     assert(from_vertex_id < next_vertex_id_);
     assert(to_vertex_id >= 0);
     assert(to_vertex_id < next_vertex_id_);
+    assert(!has_edge(from_vertex_id, to_vertex_id));
 
     const auto edge_id = generate_edge_id();
     edges_.insert(
         std::make_pair(edge_id, Edge(edge_id, from_vertex_id, to_vertex_id)));
     connetions_[from_vertex_id].insert(edge_id);
     connetions_[to_vertex_id].insert(edge_id);
+  }
+
+  bool has_edge(Vertex::Id from_vertex_id, Vertex::Id to_vertex_id) {
+    const auto to_vertices = get_connected_vertices(from_vertex_id);
+    return to_vertices.find(to_vertex_id) != to_vertices.end();
+  }
+
+  std::unordered_set<Vertex::Id> get_connected_vertices(
+      Vertex::Id from_vertex_id) {
+    std::unordered_set<Vertex::Id> result;
+    if (connetions_.find(from_vertex_id) == connetions_.end()) {
+      return result;
+    }
+    for (const auto& edge_id : connetions_[from_vertex_id]) {
+      const Edge& edge = edges_.at(edge_id);
+      if (edge.from_vertex_id() == from_vertex_id) {
+        result.insert(edge.to_vertex_id());
+      } else {
+        result.insert(edge.from_vertex_id());
+      }
+    }
+    return result;
   }
 
  private:
