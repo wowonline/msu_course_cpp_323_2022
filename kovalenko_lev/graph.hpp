f4f1244dc845acbe862b23178da4cb4996505ea2
klevvit <kle8it@gmail.com>
2022-10-09 17:23:21 +0600

    change auto -> const auto

--- kovalenko_lev/graph.hpp
+++ kovalenko_lev/graph.hpp
@@ -10,12 +10,12 @@
 class Graph {
  public:
   void add_vertex() {
-    auto vertex_id = generate_vertex_id();
+    const auto vertex_id = generate_vertex_id();
     vertices_.emplace(vertex_id, vertex_id);
   }
 
   void add_edge(Vertex::Id from_vertex_id, Vertex::Id to_vertex_id) {
-    auto edge_id = generate_edge_id();
+    const auto edge_id = generate_edge_id();
     edges_.emplace(std::piecewise_construct, std::make_tuple(edge_id),
                    std::make_tuple(edge_id, from_vertex_id, to_vertex_id));
     connetions_[from_vertex_id].insert(edge_id);
