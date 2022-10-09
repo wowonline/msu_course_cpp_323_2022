49862d60fac969df1cca30562bfcbdba81a1af8a
klevvit <kle8it@gmail.com>
2022-10-09 18:14:10 +0600

    fix adding and element to egdes_

--- kovalenko_lev/graph.hpp
+++ kovalenko_lev/graph.hpp
@@ -22,7 +22,8 @@ class Graph {
     assert(to_vertex_id < next_vertex_id_);
 
     const auto edge_id = generate_edge_id();
-    edges_[edge_id] = Edge(edge_id, from_vertex_id, to_vertex_id);
+    edges_.insert(
+        std::make_pair(edge_id, Edge(edge_id, from_vertex_id, to_vertex_id)));
     connetions_[from_vertex_id].insert(edge_id);
     connetions_[to_vertex_id].insert(edge_id);
   }
