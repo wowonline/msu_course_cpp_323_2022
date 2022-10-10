76bb057a98426761e525b7d53942e8f328e34f34
klevvit <kle8it@gmail.com>
2022-10-10 18:52:53 +0600

    remove copy constructor

--- kovalenko_lev/edge.hpp
+++ kovalenko_lev/edge.hpp
@@ -10,7 +10,6 @@ struct Edge {
 
   Edge(Id id, Vertex::Id from_vertex_id, Vertex::Id to_vertex_id)
       : id_(id), from_vertex_id_(from_vertex_id), to_vertex_id_(to_vertex_id) {}
-  Edge(const Edge& other) = default;
   Id id() const { return id_; }
   Vertex::Id from_vertex_id() const { return from_vertex_id_; }
   Vertex::Id to_vertex_id() const { return to_vertex_id_; }
