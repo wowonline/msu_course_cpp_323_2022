250c43ae965b41de7784f58f554a8c9c9bd21bbc
Huszar <yaroslavfedotov22@yandex.ru>
2022-10-14 17:25:22 +0000

    replace vectors with unordered maps

--- fedotov_yaroslav/printing.cpp
+++ fedotov_yaroslav/printing.cpp
@@ -5,26 +5,25 @@
 
 namespace printing {
 namespace json {
+
 std::string print_vertex(const Graph::Vertex& vertex, const Graph& graph) {
   std::ostringstream vertex_repr;
   bool is_first = true;
+  auto edges_ids_of_vertex = graph.adjacency_list().at(vertex.id());
   vertex_repr << "\n{ \"id\": " << vertex.id() << ", \"edges_ids\": [";
-  for (const auto& edge : graph.edges()) {
-    if (edge.from_vertex_id() == vertex.id() ||
-        edge.to_vertex_id() == vertex.id()) {
+  for (const auto& edge_id : edges_ids_of_vertex) {
       if (!is_first)
-        vertex_repr << ", " << edge.id();
+        vertex_repr << ", " << edge_id;
       else {
-        vertex_repr << edge.id();
+        vertex_repr << edge_id;
         is_first = false;
       }
-    }
   }
   vertex_repr << "] }";
   return vertex_repr.str();
 }
 
-std::string print_edge(const Graph::Edge& edge, const Graph& graph) {
+std::string print_edge(const Graph::Edge& edge) {
   std::ostringstream edge_repr;
   edge_repr << "\n{\"id\": " << edge.id() << ", \"vertex_ids\": ["
             << edge.from_vertex_id() << ", " << edge.to_vertex_id() << "]}";
@@ -34,7 +33,7 @@ std::string print_edge(const Graph::Edge& edge, const Graph& graph) {
 std::string print_graph(const Graph& graph) {
   std::string json_string = "{\"vertices\": [";
   bool is_first = true;
-  for (const auto& vertex : graph.vertices()) {
+  for (const auto& [_, vertex] : graph.vertices()) {
     if (!is_first) {
       json_string.append(", ");
     } else {
@@ -42,29 +41,20 @@ std::string print_graph(const Graph& graph) {
     }
     json_string.append(print_vertex(vertex, graph));
   }
+
   json_string.append("],\n\"edges\": [");
   is_first = true;
-  for (const auto& edge : graph.edges()) {
+  for (const auto& [_, edge] : graph.edges()) {
     if (!is_first) {
       json_string.append(", ");
     } else {
       is_first = false;
     }
-    json_string.append(print_edge(edge, graph));
+    json_string.append(print_edge(edge));
   }
   json_string.append("]}\n");
   return json_string;
 }
 };  // namespace json
-
-void write_to_file(const std::string graph_repr, const std::string file_name) {
-  std::ofstream output(file_name);
-  if (output.is_open()) {
-    output << graph_repr;
-    output.close();
-  } else {
-    std::cout << "Unable to open file";
-  }
-}
 
 };  // namespace printing
--- fedotov_yaroslav/printing.hpp
+++ fedotov_yaroslav/printing.hpp
@@ -6,9 +6,9 @@
 namespace printing {
 namespace json {
 std::string print_vertex(const Graph::Vertex& vertex, const Graph& graph);
+
 std::string print_edge(const Graph::Edge& edge, const Graph& graph);
+
 std::string print_graph(const Graph& graph);
 }  // namespace json
-
-void write_to_file(const std::string graph_repr, const std::string file_name);
 }  // namespace printing
