b69292003dcec5090133e7f5f729c71d897ac104
Huszar <yaroslavfedotov22@yandex.ru>
2022-10-14 17:49:27 +0000

    Revert "replace vectors with unordered maps"
    This reverts commit cd3639d8ed607c22467c850c01b3b614003fb989.

--- fedotov_yaroslav/graph.cpp
+++ fedotov_yaroslav/graph.cpp
@@ -3,27 +3,87 @@
 #include <iostream>
 #include "printing.hpp"
 
-
 void Graph::add_vertex() {
-  VertexId new_id = get_new_vertex_id();
-  vertices_.emplace(new_id, new_id);
-  adjacency_list_.emplace(new_id, std::vector<EdgeId>{});
+  vertices_.emplace_back(get_new_vertex_id());
 }
 
 void Graph::add_edge(VertexId first_vertex_id, VertexId second_vertex_id) {
   assert(has_vertex(first_vertex_id));
   assert(has_vertex(second_vertex_id));
-  EdgeId new_id = get_new_edge_id();
-
-  edges_.try_emplace(new_id, new_id, first_vertex_id, second_vertex_id);
-  adjacency_list_[first_vertex_id].emplace_back(new_id);
-  adjacency_list_[second_vertex_id].emplace_back(new_id);
+  edges_.emplace_back(get_new_edge_id(), first_vertex_id, second_vertex_id);
 }
 
 bool Graph::has_vertex(VertexId id) const {
-  return vertices_.find(id) != vertices_.end();
+  for (const auto& vertex : vertices_) {
+    if (vertex.id() == id) {
+      return true;
+    }
+  }
+  return false;
 }
 
+Graph::VertexId Graph::get_new_vertex_id() {
+  return last_vertex_id_++;
+}
 
+Graph::EdgeId Graph::get_new_edge_id() {
+  return last_edge_id_++;
+}
 
+Graph::Vertex::Vertex(VertexId id) : id_(id) {}
+Graph::VertexId Graph::Vertex::id() const {
+  return id_;
+}
 
+Graph::Edge::Edge(EdgeId id, VertexId from_vertex_id, VertexId to_vertex_id)
+    : id_(id), from_vertex_id_(from_vertex_id), to_vertex_id_(to_vertex_id) {}
+
+Graph::EdgeId Graph::Edge::id() const {
+  return id_;
+}
+Graph::VertexId Graph::Edge::from_vertex_id() const {
+  return from_vertex_id_;
+}
+Graph::VertexId Graph::Edge::to_vertex_id() const {
+  return to_vertex_id_;
+}
+
+const std::vector<Graph::Vertex>& Graph::vertices() const {
+  return vertices_;
+}
+const std::vector<Graph::Edge>& Graph::edges() const {
+  return edges_;
+}
+
+constexpr int kVerticesCount = 14;
+
+int main() {
+  auto graph = Graph();
+
+  for (int i = 0; i < kVerticesCount; i++) {
+    graph.add_vertex();
+  }
+
+  graph.add_edge(0, 1);
+  graph.add_edge(0, 2);
+  graph.add_edge(0, 3);
+  graph.add_edge(1, 4);
+  graph.add_edge(1, 5);
+  graph.add_edge(1, 6);
+  graph.add_edge(2, 7);
+  graph.add_edge(2, 8);
+  graph.add_edge(3, 9);
+  graph.add_edge(4, 10);
+  graph.add_edge(5, 10);
+  graph.add_edge(6, 10);
+  graph.add_edge(7, 11);
+  graph.add_edge(8, 11);
+  graph.add_edge(9, 12);
+  graph.add_edge(10, 13);
+  graph.add_edge(11, 13);
+  graph.add_edge(12, 13);
+
+  const auto graph_json = printing::json::print_graph(graph);
+  std::cout << graph_json << std::endl;
+  printing::write_to_file(graph_json, "graph.json");
+}
--- fedotov_yaroslav/graph.hpp
+++ fedotov_yaroslav/graph.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include <unordered_map>
 #include <vector>
 
 class Graph {
@@ -9,30 +8,18 @@ class Graph {
 
   struct Vertex {
    public:
-    explicit Vertex(VertexId id): id_(id){};
-    VertexId id() const {
-      return id_;
-    };
+    explicit Vertex(VertexId id);
+    VertexId id() const;
 
    private:
     VertexId id_ = 0;
   };
   struct Edge {
    public:
-    Edge(EdgeId id, VertexId from_vertex_id, VertexId to_vertex_id)
-      : id_(id), from_vertex_id_(from_vertex_id), to_vertex_id_(to_vertex_id){};
-
-    EdgeId id() const {
-      return id_;
-    };
-
-    VertexId from_vertex_id() const {
-      return from_vertex_id_;
-    };
-
-    VertexId to_vertex_id() const {
-      return to_vertex_id_;
-    };
+    Edge(EdgeId id, VertexId from_vertex_id, VertexId to_vertex_id);
+    EdgeId id() const;
+    VertexId from_vertex_id() const;
+    VertexId to_vertex_id() const;
 
    private:
     EdgeId id_ = 0;
@@ -41,35 +28,17 @@ class Graph {
   };
 
   void add_vertex();
-
   void add_edge(VertexId first_vertex_id, VertexId second_vertex_id);
-
-  const std::unordered_map<VertexId, Vertex>& vertices() const {
-    return vertices_;
-  };
-
-  const std::unordered_map<EdgeId, Edge>& edges() const {
-    return edges_;
-  };
-
-  const std::unordered_map<VertexId, std::vector<EdgeId>>& adjacency_list() const {
-    return adjacency_list_;
-  };
+  const std::vector<Vertex>& vertices() const;
+  const std::vector<Edge>& edges() const;
 
  private:
   bool has_vertex(VertexId id) const;
+  VertexId get_new_vertex_id();
+  EdgeId get_new_edge_id();
 
-  VertexId get_new_vertex_id() {
-    return last_vertex_id_++;
-  };
-
-  EdgeId get_new_edge_id() {
-    return last_edge_id_++;
-  };
-
-  std::unordered_map<VertexId, std::vector<EdgeId>> adjacency_list_;
-  std::unordered_map<VertexId, Vertex> vertices_;
-  std::unordered_map<EdgeId, Edge> edges_;
+  std::vector<Vertex> vertices_;
+  std::vector<Edge> edges_;
   VertexId last_vertex_id_ = 0;
   EdgeId last_edge_id_ = 0;
 };
