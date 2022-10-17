947a2447c7a629caf38ca4a2cb21353e11662d3d
Kuzminskiy Andrey <s02200417@gse.cs.msu.ru>
2022-10-17 13:18:57 +0300

    Colors

--- kuzminskiy_stafeev/classes.cpp
+++ kuzminskiy_stafeev/classes.cpp
@@ -25,11 +25,8 @@ Graph::VertexId Graph::add_vertex() {
 
 bool Graph::is_connected(const Graph::VertexId& from_vertex_id,
                          const Graph::VertexId& to_vertex_id) const {
-  auto PullEdgesFrom = connections_list_.at(from_vertex_id);
-  auto PullEdgesTo = connections_list_.at(to_vertex_id);
-
-  // std::sort(PullEdgesFrom.begin(), PullEdgesFrom.end());
-  // std::sort(PullEdgesTo.begin(), PullEdgesTo.end());
+  const auto& PullEdgesFrom = connections_list_.at(from_vertex_id);
+  const auto& PullEdgesTo = connections_list_.at(to_vertex_id);
 
   std::set<Graph::EdgeId> intersection;
 
