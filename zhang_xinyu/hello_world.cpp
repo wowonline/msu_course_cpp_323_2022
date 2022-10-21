dab40e506dd0f00da68b597a3a8f1eb4acbda63b
YamyuCheung <s02190797@gse.cs.msu.ru>
2022-10-21 19:20:33 +0300

    task_02_2

--- zhang_xinyu/hello_world.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-  std::cout << "Hello World!" << std::endl;
-  std::cout << "Zhang Xinyu" << std::endl;
-  std::cout << "Чжан Синьюй" << std::endl;
-  return 0;
-}
