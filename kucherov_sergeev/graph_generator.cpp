d0bdd29e62425dfa0321b07f9f48d19e7c3cb2d4
Basil Kucherov <basilikgreat@gmail.com>
2022-10-21 21:04:04 +0300

    add GraphGenerator class

--- kucherov_sergeev/graph_generator.cpp
+++ kucherov_sergeev/graph_generator.cpp
@@ -76,6 +76,36 @@ class Graph {
   std::unordered_map<VertexId, std::vector<EdgeId>> adjacency_list_;
 };
 
+class GraphGenerator {
+ public:
+  struct Params {
+   public:
+    Params(Graph::Depth depth, int new_vertices_count)
+        : depth_(depth), new_vertices_count_(new_vertices_count) {}
+
+    Graph::Depth depth() const { return depth_; }
+    int new_vertices_count() const { return new_vertices_count_; }
+
+   private:
+    Graph::Depth depth_ = 0;
+    int new_vertices_count_ = 0;
+  };
+
+  explicit GraphGenerator(Params&& params) : params_(std::move(params)) {}
+
+  Graph generate() const {
+    auto graph = Graph();
+    graph.add_vertex();
+    generate_grey_edges(graph);
+    generate_green_edges(graph);
+    generate_yellow_edges(graph);
+    generate_red_edges(graph);
+  }
+
+ private:
+  Params params_ = Params(0, 0);
+};
+
 namespace printing {
 namespace json {
 
