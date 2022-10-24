7ce62808b151e1e7ede8e16b0005fb15e2e14f64
KuzAlex <kkuzaalexey@gmail.com>
2022-10-24 12:48:58 +0300

    Add hello_world app

---
+++ kuznetsov_alexey/hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "Kuznetsov Alexey" << std::endl;
+  std::cout << "Кузнецов Алексей" << std::endl;
+  return 0;
+}
