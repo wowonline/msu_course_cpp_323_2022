b35f97aaba97028ae9ad61e794eacf4c8011c0d5
YamyuCheung <s02190797@gse.cs.msu.ru>
2022-10-24 09:45:27 +0300

    chg

--- zhang_xinyu/graph.cpp
+++ zhang_xinyu/graph.cpp
@@ -7,9 +7,18 @@ class Graph {
   using VertexId = int;
   using EdgeId = int;
   void add_vertex();
-  void add_edge(VertexId from_vertex_id, VertexId to_vertex_id);
+  void add_edge(VertexId from_vertex_id, VertexId to_vertex_id) {
+    assert(has_vertex(from_vertex_id));
+    assert(has_vertex(to_vertex_id));
+    edges_.emplace_back(get_new_vertex_id(), from_vertex_id, to_vertex_id);
+  }
 
  private:
+  bool has_vertex(VertexId id) const {
+    return std::any_of(
+        vertexes_.begin(), vertexes_.end(),
+        [id](const Vertex& vertex) { return vertex.id() == id; });
+  }
   struct Vertex {
    public:
     explicit Vertex(VertexId id) : id_(id) {}
@@ -44,20 +53,8 @@ class Graph {
   EdgeId get_new_edge_id() { return last_edge_id_++; }
 };
 
-// bool has_vertex(Graph::VertexId id) const{
-//     return std::any_of(
-//            vertexes_.begin(), vertexes_.end(),
-//            [id](const Vertex& vertex) { return vertex.id() == id; });
-// }
-
 void Graph::add_vertex() {
   vertexes_.emplace_back(get_new_vertex_id());
-}
-
-void Graph::add_edge(VertexId from_vertex_id, VertexId to_vertex_id) {
-  //  assert(has_vertex(from_vertex_id));
-  //  assert(has_vertex(to_vertex_id));
-  edges_.emplace_back(get_new_vertex_id(), from_vertex_id, to_vertex_id);
 }
 
 constexpr int kVerticesCount = 14;
