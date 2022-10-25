f739f9a0963324c356e9e459141fadbf5cf8a8f0
klevvit <kle8it@gmail.com>
2022-10-25 22:30:04 +0600

    Move generate_graph function contents

--- tsybina_kovalenko/graph_generator.hpp
+++ tsybina_kovalenko/graph_generator.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+// #include <queue>
+
 #include "graph.hpp"
 
 class GraphGenerator {
@@ -21,18 +23,61 @@ class GraphGenerator {
 
   Graph generate() const {
     auto graph = Graph();
+
+    constexpr int kVerticesCount = 14;
+
+    for (int i = 0; i < kVerticesCount; i++) {
+      graph.add_vertex();
+    }
+
+    graph.add_edge(0, 1);
+    graph.add_edge(0, 2);
+    graph.add_edge(0, 3);
+    graph.add_edge(1, 4);
+    graph.add_edge(1, 5);
+    graph.add_edge(1, 6);
+    graph.add_edge(2, 7);
+    graph.add_edge(2, 8);
+    graph.add_edge(3, 9);
+    graph.add_edge(4, 10);
+    graph.add_edge(5, 10);
+    graph.add_edge(6, 10);
+    graph.add_edge(7, 11);
+    graph.add_edge(8, 11);
+    graph.add_edge(9, 12);
+    graph.add_edge(10, 13);
+    graph.add_edge(11, 13);
+    graph.add_edge(12, 13);
+
+    return graph;
+
+    /*
     graph.add_vertex();
+
     generate_grey_edges(graph);
     generate_green_edges(graph);
     generate_yellow_edges(graph);
     generate_red_edges(graph);
+    return graph;
+    */
   }
 
  private:
   Params params_ = Params(0, 0);
 
-  void generate_grey_edges(Graph& graph) const;
-  void generate_green_edges(Graph& graph) const;
-  void generate_yellow_edges(Graph& graph) const;
-  void generate_red_edges(Graph& graph) const;
+  void generate_grey_edges(Graph& graph) const { /* TODO */
+    /*
+      if (params_.depth() == 0) {
+       return;
+     }
+     std::queue<std::reference_wrapper<const Graph::Vertex>> queue;
+     queue.push(graph.vertices().at(0));
+    */
+  }
+  void generate_green_edges(Graph& graph) const { /* TODO */
+  }
+  void generate_yellow_edges(Graph& graph) const { /* TODO */
+  }
+  void generate_red_edges(Graph& graph) const { /* TODO */
+  }
 };
--- tsybina_kovalenko/main.cpp
+++ tsybina_kovalenko/main.cpp
@@ -2,37 +2,8 @@
 #include <iostream>
 
 #include "graph.hpp"
+#include "graph_generator.hpp"
 #include "printing.hpp"
-
-Graph generate_graph() {
-  auto graph = Graph();
-  constexpr int kVerticesCount = 14;
-
-  for (int i = 0; i < kVerticesCount; i++) {
-    graph.add_vertex();
-  }
-
-  graph.add_edge(0, 1);
-  graph.add_edge(0, 2);
-  graph.add_edge(0, 3);
-  graph.add_edge(1, 4);
-  graph.add_edge(1, 5);
-  graph.add_edge(1, 6);
-  graph.add_edge(2, 7);
-  graph.add_edge(2, 8);
-  graph.add_edge(3, 9);
-  graph.add_edge(4, 10);
-  graph.add_edge(5, 10);
-  graph.add_edge(6, 10);
-  graph.add_edge(7, 11);
-  graph.add_edge(8, 11);
-  graph.add_edge(9, 12);
-  graph.add_edge(10, 13);
-  graph.add_edge(11, 13);
-  graph.add_edge(12, 13);
-
-  return graph;
-}
 
 void write_to_file(const std::string& data, const std::string& file_path) {
   std::ofstream out(file_path, std::ios::out | std::ios::trunc);
@@ -41,7 +12,7 @@ void write_to_file(const std::string& data, const std::string& file_path) {
 }
 
 int main() {
-  const auto graph = generate_graph();
+  const auto graph = GraphGenerator(GraphGenerator::Params(0, 0)).generate();
   const auto graph_json = printing::json::print_graph(graph);
   std::cout << graph_json << std::endl;
   write_to_file(graph_json, "graph.json");
