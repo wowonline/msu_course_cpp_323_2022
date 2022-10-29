bbd430aab56d2f7791825b8e8eb612a2ad863855
klevvit <kle8it@gmail.com>
2022-10-29 13:37:29 +0600

    implement vertex_depths_

--- tsybina_kovalenko/main.cpp
+++ tsybina_kovalenko/main.cpp
@@ -13,9 +13,13 @@ void write_to_file(const std::string& data, const std::string& file_path) {
 
 int main() {
   const auto graph = GraphGenerator(GraphGenerator::Params(0, 0)).generate();
-  const auto graph_json = printing::json::print_graph(graph);
-  std::cout << graph_json << std::endl;
-  write_to_file(graph_json, "graph.json");
+  for (int vertex_id = 0; vertex_id < graph.vertices().size(); ++vertex_id) {
+    std::cout << vertex_id << ": " << graph.vertex_depths().at(vertex_id)
+              << std::endl;
+  }
+  // const auto graph_json = printing::json::print_graph(graph);
+  // std::cout << graph_json << std::endl;
+  // write_to_file(graph_json, "graph.json");
 
   return 0;
 }
