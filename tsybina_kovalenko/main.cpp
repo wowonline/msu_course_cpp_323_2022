55720fd4c8d9c6b441f4ec9b19723a7d4b39f949
klevvit <kle8it@gmail.com>
2022-10-29 13:56:09 +0600

    add ask_params()

--- tsybina_kovalenko/main.cpp
+++ tsybina_kovalenko/main.cpp
@@ -11,8 +11,36 @@ void write_to_file(const std::string& data, const std::string& file_path) {
   out.close();
 }
 
+GraphGenerator::Params ask_params() {
+  Graph::Depth depth;
+  bool is_depth_correct = false;
+  do {
+    std::cout << "Enter graph depth: " << std::flush;
+    std::cin >> depth;
+    is_depth_correct = depth >= 0;
+    if (!is_depth_correct) {
+      std::cout << "Alas, depth can't be negative :(\n";
+    }
+  } while (!is_depth_correct);
+
+  int new_vertices_count;
+  bool is_new_vertices_count_correct = false;
+  do {
+    std::cout << "Enter new vertices count for each vertex: " << std::flush;
+    std::cin >> new_vertices_count;
+    is_new_vertices_count_correct = new_vertices_count >= 0;
+    if (!is_new_vertices_count_correct) {
+      std::cout << "Alas, new vertices count can't be negative :(\n";
+    }
+  } while (!is_new_vertices_count_correct);
+
+  return {depth, new_vertices_count};
+}
+
 int main() {
-  const auto graph = GraphGenerator(GraphGenerator::Params(0, 0)).generate();
+  auto params = ask_params();
+
+  const auto graph = GraphGenerator(std::move(params)).generate();
   for (int vertex_id = 0; vertex_id < graph.vertices().size(); ++vertex_id) {
     std::cout << vertex_id << ": " << graph.vertex_depths().at(vertex_id)
               << std::endl;
