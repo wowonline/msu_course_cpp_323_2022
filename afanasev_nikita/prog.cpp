85456330bb8142c6e09c966610674f113f3fb997
Afanasev Nikita <nikitanmlk@gmail.com>
2022-10-31 12:54:32 +0300

    fixing bugs

--- afanasev_nikita/prog.cpp
+++ afanasev_nikita/prog.cpp
@@ -291,7 +291,7 @@ class GraphGenerator {
     }
   }
   void generate_red_edges(Graph& graph) const {
-    for (auto depth = 1; depth < params_.get_depth() - 1; ++depth) {
+    for (auto depth = 1; depth < params_.get_depth() - 2; ++depth) {
       std::random_device device_1, device_2;
       std::mt19937 generator_for_bernoulli_distribution(device_1()),
           generator_for_uniform_integer_distribution(device_2());
