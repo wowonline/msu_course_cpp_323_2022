0fbaef1d38861d53d09d94c88883dc628e188f94
Jedi314 <olga.tsybina82@gmail.com>
2022-11-02 21:24:38 +0000

    fix input errors check & split function

--- tsybina_kovalenko/main.cpp
+++ tsybina_kovalenko/main.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <string>
 
 #include "graph.hpp"
 #include "graph_generator.hpp"
@@ -11,36 +12,68 @@ void write_to_file(const std::string& data, const std::string& file_path) {
   out.close();
 }
 
-GraphGenerator::Params handle_params_input() {
+bool is_number(std::string str) {
+  for (auto it = str.begin(); it != str.end(); ++it) {
+    if (*it == '-')
+      continue;
+    if (isdigit(*it) == false) {
+      return false;
+    }
+  }
+  return true;
+}
+
+Graph::Depth handle_depth_input() {
   Graph::Depth depth;
   bool is_depth_correct = false;
   do {
     std::cout << "Enter graph depth: " << std::flush;
-    std::cin >> depth;
-    is_depth_correct = depth >= 0;
-    if (!is_depth_correct) {
-      std::cout << "Alas, depth can't be negative :(\n";
+    std::string depth_string;
+    std::cin >> depth_string;
+    try {
+      depth = stoi(depth_string);
+      if (!is_number(depth_string))
+        throw std::invalid_argument("");
+      is_depth_correct = depth >= 0;
+      if (!is_depth_correct) {
+        std::cout << "Alas, depth can't be negative :(" << std::endl;
+      }
+    } catch (const std::invalid_argument&) {
+      std::cout << "Alas, depth should be a number :(" << std::endl;
     }
   } while (!is_depth_correct);
+  return depth;
+}
 
+int handle_new_vertices_count_input() {
   int new_vertices_count;
   bool is_new_vertices_count_correct = false;
   do {
     std::cout << "Enter new vertices count for each vertex: " << std::flush;
-    std::cin >> new_vertices_count;
-    is_new_vertices_count_correct = new_vertices_count >= 0;
-    if (!is_new_vertices_count_correct) {
-      std::cout << "Alas, new vertices count can't be negative :(\n";
+    std::string new_vertices_count_string;
+    std::cin >> new_vertices_count_string;
+    try {
+      new_vertices_count = stoi(new_vertices_count_string);
+      if (!is_number(new_vertices_count_string))
+        throw std::invalid_argument("");
+      is_new_vertices_count_correct = new_vertices_count >= 0;
+      if (!is_new_vertices_count_correct) {
+        std::cout << "Alas, new vertices count can't be negative :("
+                  << std::endl;
+      }
+    } catch (const std::invalid_argument&) {
+      std::cout << "Alas, new vertices count should be a number :("
+                << std::endl;
     }
   } while (!is_new_vertices_count_correct);
-
-  return {depth, new_vertices_count};
+  return new_vertices_count;
 }
 
 int main() {
-  auto params = handle_params_input();
+  auto depth = handle_depth_input();
+  auto new_vertices_count = handle_new_vertices_count_input();
 
-  const auto graph = GraphGenerator(std::move(params)).generate();
+  const auto graph = GraphGenerator({depth, new_vertices_count}).generate();
   const auto graph_json = printing::json::print_graph(graph);
   std::cout << graph_json << std::endl;
   write_to_file(graph_json, "graph.json");
