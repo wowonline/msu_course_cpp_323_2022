70002c235e5c8811158327b31fa72a33dd076646
Basil Kucherov <basilikgreat@gmail.com>
2022-11-04 10:08:03 +0300

    add handle_graphs_count_input function and multy graph generation

--- kucherov_sergeev/main.cpp
+++ kucherov_sergeev/main.cpp
@@ -63,20 +63,50 @@ int handle_new_vertices_count_input() {
 
   return new_vertices_count;
 }
+
+int handle_graphs_count_input() {
+  const std::string init_message = "Type new grpahs count: ";
+  const std::string err_format_message =
+      "Graphs count must be a non-negative integer. Try again";
+  int graphs_count;
+  int correct_input = false;
+
+  std::cout << init_message << std::endl;
+
+  while (correct_input == false) {
+    if (std::cin >> graphs_count && graphs_count >= 0) {
+      correct_input = true;
+    } else if (std::cin.fail() || graphs_count < 0) {
+      std::cout << err_format_message << std::endl;
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    } else {
+      throw std::runtime_error("Failed to get new graphs count from user");
+    }
+  }
+
+  return graphs_count;
+}
 }  // namespace
 
 int main() {
   const int depth = handle_depth_input();
   const int new_vertices_count = handle_new_vertices_count_input();
+  const int graphs_count = handle_graphs_count_input();
 
   auto params =
       uni_course_cpp::GraphGenerator::Params(depth, new_vertices_count);
-  const auto generator = uni_course_cpp::GraphGenerator(std::move(params));
-  const auto graph = generator.generate();
 
-  const auto graph_json = uni_course_cpp::printing::json::print_graph(graph);
-  std::cout << graph_json << std::endl;
-  write_to_file(graph_json, "graph.json");
+  const auto generator = uni_course_cpp::GraphGenerator(std::move(params));
+
+  for (int i = 0; i < graphs_count; i++) {
+    const auto graph = generator.generate();
+
+    const auto graph_json = uni_course_cpp::printing::json::print_graph(graph);
+    std::cout << graph_json << std::endl;
+
+    write_to_file(graph_json, "./temp/graph_" + std::to_string(i) + ".json");
+  }
 
   return 0;
 }
