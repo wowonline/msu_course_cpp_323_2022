6b158a74d590344fd5cb969dab8cbec0b009b155
Jedi314 <olga.tsybina82@gmail.com>
2022-11-06 19:34:27 +0000

    improve generate_red_edges productivity

--- tsybina_kovalenko/graph_generator.hpp
+++ tsybina_kovalenko/graph_generator.hpp
@@ -25,12 +25,12 @@ class GraphGenerator {
   Graph generate() const {
     auto graph = Graph();
     if (params_.vertex_depth() != 0) {
-    graph.add_vertex();
+      graph.add_vertex();
 
-    generate_grey_edges(graph);
-    generate_green_edges(graph);
-    generate_yellow_edges(graph);
-    generate_red_edges(graph);
+      generate_grey_edges(graph);
+      generate_green_edges(graph);
+      generate_yellow_edges(graph);
+      generate_red_edges(graph);
     }
 
     return graph;
@@ -97,9 +97,9 @@ class GraphGenerator {
     for (Graph::Depth vertex_depth = kYellowEdgeStartDepth;
          vertex_depth <= params_.vertex_depth() - kYellowEdgeDepthJump;
          ++vertex_depth) {
+      const float success_chance =
+          static_cast<float>(vertex_depth - 1) / (params_.vertex_depth() - 2);
       for (Graph::VertexId vertex_id : graph.vertices_at_depth(vertex_depth)) {
-        const float success_chance =
-            static_cast<float>(vertex_depth - 1) / (params_.vertex_depth() - 2);
         if (check_probability(success_chance)) {
           add_yellow_edge(graph, vertex_id);
         }
@@ -125,26 +125,25 @@ class GraphGenerator {
   }
 
   void generate_red_edges(Graph& graph) const {
-    for (const auto& [vertex_id, vertex] : graph.vertices()) {
-      const auto vertex_depth = graph.depth_of(vertex_id);
-      if (vertex_depth <= params_.vertex_depth() - kRedEdgeDepthJump) {
-        if (check_probability(kRedEdgeGenerationChance)) {
-          add_red_edge(graph, vertex_id);
+    for (Graph::Depth vertex_depth = 1;
+         vertex_depth <= params_.vertex_depth() - kRedEdgeDepthJump;
+         ++vertex_depth) {
+      const auto& probable_vertices_unordered_set =
+          graph.vertices_at_depth(vertex_depth + kRedEdgeDepthJump);
+
+      if (probable_vertices_unordered_set.size() > 0) {
+        std::vector<Graph::VertexId> probable_vertices{
+            probable_vertices_unordered_set.begin(),
+            probable_vertices_unordered_set.end()};
+
+        for (Graph::VertexId from_vertex_id :
+             graph.vertices_at_depth(vertex_depth)) {
+          if (check_probability(kRedEdgeGenerationChance)) {
+            graph.add_edge(from_vertex_id,
+                           select_random_vertex(probable_vertices));
+          }
         }
       }
-    }
-  }
-
-  void add_red_edge(Graph& graph, Graph::VertexId from_vertex_id) const {
-    std::vector<Graph::VertexId> probable_vertices;
-    for (const auto& [vertex_id, vertex] : graph.vertices()) {
-      if (graph.depth_of(vertex_id) ==
-          graph.depth_of(from_vertex_id) + kRedEdgeDepthJump) {
-        probable_vertices.push_back(vertex_id);
-      }
-    }
-    if (probable_vertices.size() > 0) {
-      graph.add_edge(from_vertex_id, select_random_vertex(probable_vertices));
     }
   }
 };
