8978fd252e2325271d3a9f0907451fe9e9add944
KuzAlex <kkuzaalexey@gmail.com>
2022-11-17 20:31:52 +0300

    add graph

---
+++ kuznetsov_alexey/graph.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <algorithm>
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <random>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+
+class Graph {
+ public:
+  using VertexId = int;
+  using EdgeId = int;
+  using Depth = int;
+
+  struct Vertex {
+   public:
+    explicit Vertex(VertexId id) : id_(id) {}
+    VertexId id() const { return id_; }
+    Depth depth() const { return depth_; }
+    void set_depth(Depth depth) { depth_ = depth; }
+
+   private:
+    Depth depth_ = 1;
+    VertexId id_ = 0;
+  };
+
+  struct Edge {
+    enum class Color { Grey, Green, Yellow, Red };
+
+   public:
+    Edge(EdgeId id, VertexId from_vertex_id, VertexId to_vertex_id, Color color)
+        : id_(id),
+          from_vertex_id_(from_vertex_id),
+          to_vertex_id_(to_vertex_id),
+          color_(color) {}
+    EdgeId id() const { return id_; }
+    VertexId from_vertex_id() const { return from_vertex_id_; }
+    VertexId to_vertex_id() const { return to_vertex_id_; }
+    Color color() const { return color_; }
+
+   private:
+    Color color_ = Color::Grey;
+    EdgeId id_ = 0;
+    VertexId from_vertex_id_ = 0;
+    VertexId to_vertex_id_ = 0;
+  };
+
+  const std::vector<Edge>& get_edges() const { return edges_; }
+
+  const std::vector<Vertex>& get_vertices() const { return vertices_; }
+
+  void add_vertex();
+
+  void set_vertex_depth(VertexId vertex_id, Depth depth);
+
+  bool has_vertex(VertexId vertex_id) const { return adjacency_list_.count(vertex_id); }
+
+  bool is_connected(VertexId from_vertex_id, VertexId to_vertex_id) const;
+
+  const std::vector<EdgeId>& connected_edges_ids(VertexId vertex_id) const;
+
+  Edge::Color get_edge_color(VertexId from_vertex_id,
+                             VertexId to_vertex_id) const;
+
+  void add_edge(VertexId from_vertex_id, VertexId to_vertex_id);
+
+  std::vector<Vertex> get_vertices_with_depth(Depth depth) const;
+
+  Depth depth() const { return graph_depth_; }
+
+ private:
+ 
+  VertexId get_new_vertex_id_() { return vertex_id_counter_++; }
+  EdgeId get_new_edge_id_() { return edge_id_counter_++; }
+
+  Depth graph_depth_ = 0;
+  VertexId vertex_id_counter_ = 0;
+  EdgeId edge_id_counter_ = 0;
+  std::unordered_map<Depth, std::vector<Vertex> > depth_to_vertices_;
+  std::unordered_map<VertexId, std::vector<EdgeId> > adjacency_list_of_edges_;
+  std::unordered_map<VertexId, std::vector<VertexId> > adjacency_list_;
+  std::vector<Vertex> vertices_;
+  std::vector<Edge> edges_;
+};
---
+++ kuznetsov_alexey/graph_generator.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "graph.hpp"
+
+// graph generator
+
+class GraphGenerator {
+ public:
+  struct Params {
+   public:
+    Params(Graph::Depth depth, int new_vertices_count)
+        : depth_(depth), new_vertices_count_(new_vertices_count) {}
+
+    Graph::Depth depth() const { return depth_; }
+    int new_vertices_count() const { return new_vertices_count_; }
+
+   private:
+    Graph::Depth depth_ = 0;
+    int new_vertices_count_ = 0;
+  };
+
+  explicit GraphGenerator(Params&& params) : params_(std::move(params)) {}
+  Graph generate() const;
+
+ private:
+  double generate_random(double a, double b) const;
+
+  double probaility_generate_grey_edge(Graph::Depth current_depth,
+                                       Graph::Depth graph_depth) const;
+
+  bool try_generate_grey_edge(Graph& graph,
+                              Graph::Depth graph_depth,
+                              Graph::Depth current_depth,
+                              Graph::VertexId vertex_id,
+                              Graph::VertexId& next_vertex_id)
+      const;  // generate grey edge: (vertex_id, next_vertex_id)
+
+  void generate_grey_edges(Graph& graph) const;
+
+  void try_generate_yellow_edge(Graph& graph,
+                                Graph::Depth graph_depth,
+                                const Graph::Vertex& vertex_from,
+                                Graph::Vertex& vertex_to) const;
+
+  void generate_yellow_edges(Graph& graph) const;
+
+  void try_generate_red_edge(Graph& graph,
+                             const Graph::Vertex& vertex_from,
+                             Graph::Vertex& vertex_to) const;
+
+  void generate_red_edges(Graph& graph) const;
+
+  void try_generate_green_edge(Graph& graph,
+                               const Graph::Vertex& vertex) const;
+
+  void generate_green_edges(Graph& graph) const;
+
+  Params params_ = Params(0, 0);
+
+};
