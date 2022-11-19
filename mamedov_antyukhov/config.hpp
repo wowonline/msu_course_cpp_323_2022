121c6adef496dce394720057831325b127e78bf7
sm0kebamb0o <mtimur_2011@mail.ru>
2022-11-20 01:39:48 +0300

    Small change in config

--- mamedov_antyukhov/config.hpp
+++ mamedov_antyukhov/config.hpp
@@ -5,7 +5,7 @@
 namespace uni_course_cpp {
 namespace config {
 
-inline constexpr const char* kTempDirectoryPath = "temp/";
+inline constexpr const char* kTempDirectoryPath = "./temp/";
 inline constexpr const char* kLogFilename = "log.txt";
 inline const std::string kLogFilePath =
     static_cast<std::string>(kTempDirectoryPath) +
