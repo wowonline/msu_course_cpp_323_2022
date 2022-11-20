97f369e433a66dde635c819f54dbdc382663e591
sm0kebamb0o <mtimur_2011@mail.ru>
2022-11-20 16:22:31 +0300

    clang-format to logger.hpp

--- mamedov_antyukhov/logger.hpp
+++ mamedov_antyukhov/logger.hpp
@@ -9,7 +9,6 @@ class Logger {
   static Logger& get_logger();
   void log(const std::string& string);
 
-
  private:
   ~Logger() = default;
   Logger();
@@ -17,7 +16,7 @@ class Logger {
   void operator=(const Logger& other) = delete;
   Logger(Logger&& other) = delete;
   void operator=(Logger&& other) = delete;
-  
+
   std::ofstream log_file_;
 };
 }  // namespace uni_course_cpp
